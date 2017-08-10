ac296980acfa161ba2dbe5c3297519de40452d7c
Nic <nic@neap.co>
2017-08-10 22:22:25 +1000

    feat: Add support for firebase functions hosting + linting + README + LICENSE

--- questions.js
+++ questions.js
@@ -5,41 +5,24 @@ const colors = require('colors')
 
 const sanitizeDest = dest => dest ? dest.split(' ').map(x => x.trim().toLowerCase()).join('') : null
 const sanitizeProjectName = name => name ? name.split(' ').join('-') : null
-const sanitizeEntryPoint = name => name ? name.split(' ').join('') : null
+const sanitizeEntryPoint = name => name ? name.split(' ').join('').replace('-','') : null
 const sanitizeFunctionName = name => name ? name.trim().split(' ').map(x => x.toLowerCase()).join('-') : null
 const sanitizeBucket = name => name ? name.trim().split(' ').map(x => x.toLowerCase()).join('-') : null
 const TRIGGERS = { '1': '--trigger-http', '2': '--trigger-topic', '3': '--trigger-bucket' }
+const HOSTING = { '1': 'googlecloud', '2': 'firebase' }
 
-exports.preQuestions = () => {
-	const gcloudNotInstalled = !shell.exec('which gcloud', {silent:true}).stdout
-	const functionsNotInstalled = !shell.exec('which functions', {silent:true}).stdout
-	if (gcloudNotInstalled) {
-		console.log(
-			`${'ERROR'.bold.italic}: The ${'gcloud SDK'.italic} seems to not be installed on your machine.\n`.yellow +
-			'You won\'t be able to use your terminal to deploy this project to your Google Cloud Account or run it locally.\n'.yellow +
-			`${'We recommend to install it (instructions here:'.yellow} ${'https://cloud.google.com/sdk/downloads'.underline.italic.blue}).\n`)
-		/*eslint-disable */
-        process.exit(1)
-        /*eslint-enable */
-	}
-	if (functionsNotInstalled) {
-		console.log(
-			(`${'ERROR'.bold.italic}: ${'Google Function Emulator'.italic} seems to not be installed on your machine.\n` +
-			'You won\'t be able to run this project on your local machine.\n' +
-			`We recommend to install it globally: ${'npm install -g @google-cloud/functions-emulator'.bold.italic}\n`).yellow)
-		/*eslint-disable */
-        process.exit(1)
-        /*eslint-enable */
-	}
-}
+exports.preQuestions = () => {}
+
+exports.onTemplateLoaded = answers => `Congratulation Master! Your new ${answers.projectName.bold} Web Server project hosted on ${answers.hosting == 'googlecloud' ? 'Google Cloud' : 'Firebase'} Function is ready.\nNext step: Run ${answers._dest ? `cd ${answers._dest}; npm install`.italic.bold : 'npm install'.italic.bold}`
 
 exports.questions = [{
 	question: answers => `project name: ${answers._dest ? `(${sanitizeDest(answers._dest)}) ` : ''} `.cyan,
 	answerName: 'projectName',
 	defaultValue: answers => answers._dest,
 	execute: {
-		validate: null,
-		onSuccess: answer => sanitizeProjectName(answer)
+		validate: answer => answer,
+		onSuccess: answer => sanitizeProjectName(answer),
+		onError: answer => `Ooch ooch Master! The project name is required. Give it a slap again. Slap slap...`
 	},
 	files: ['package.json']
 },{
@@ -48,6 +31,20 @@ exports.questions = [{
 	defaultValue: () => '1.0.0',
 	files: ['package.json']
 },{
+	question: () => ('Hosting: \n' + 
+					'  [1] Google Cloud Functions \n' +
+					'  [2] Firebase Functions \n' +
+					'Choose one of the above: ([1]) ').cyan,
+	answerName: 'hosting',
+	defaultValue: () => 1,
+	execute: {
+		validate: answer => HOSTING[answer],
+		onSuccess: answer => HOSTING[answer],
+		onError: answer => `'${answer}' is not a valid hosting option.`
+	},
+	files: ['appconfig.json']
+},{
+	skip: answers => answers.hosting == 'firebase',
 	question: answers => `Google Cloud Function name : (${sanitizeFunctionName(answers.projectName)}) `.cyan,
 	answerName: 'functionName',
 	defaultValue: answers => answers.projectName,
@@ -55,22 +52,24 @@ exports.questions = [{
 		onSuccess: answer => sanitizeFunctionName(answer)
 	},
 	files: ['appconfig.json']
-},{
-	question: () => ('Google Cloud Function trigger: \n' + 
-					'  [1] HTTP \n' +
-					'  [2] Pub/Sub \n' +
-					'  [3] Storage \n' +
-					'Choose one of the above: ([1]) ').cyan,
-	answerName: 'trigger',
-	defaultValue: () => 1,
-	execute: {
-		validate: answer => TRIGGERS[answer],
-		onSuccess: answer => TRIGGERS[answer],
-		onError: answer => `'${answer}' is not a valid trigger.`
-	},
-	files: ['appconfig.json']
-},{
-	question: answers => `Google Cloud Function entry-point (no spaces, no hyphens): (${sanitizeEntryPoint(answers.projectName)}) `.cyan,
+},
+// {
+// 	question: () => ('Google Cloud Function trigger: \n' + 
+// 					'  [1] HTTP \n' +
+// 					'  [2] Pub/Sub \n' +
+// 					'  [3] Storage \n' +
+// 					'Choose one of the above: ([1]) ').cyan,
+// 	answerName: 'trigger',
+// 	defaultValue: () => 1,
+// 	execute: {
+// 		validate: answer => TRIGGERS[answer],
+// 		onSuccess: answer => TRIGGERS[answer],
+// 		onError: answer => `'${answer}' is not a valid trigger.`
+// 	},
+// 	files: ['appconfig.json']
+// },
+{
+	question: answers => `${answers.hosting == 'googlecloud' ? 'Google Cloud' : 'Firebase'} Function entry-point (no spaces, no hyphens): (${sanitizeEntryPoint(answers.projectName)}) `.cyan,
 	answerName: 'entryPoint',
 	defaultValue: answers => answers.projectName,
 	execute: {
@@ -78,14 +77,15 @@ exports.questions = [{
 	},
 	files: ['index.js', 'appconfig.json']
 },{
-	question: answers => `Google Cloud Project: (${answers.projectName.toLowerCase()}) `.cyan,
-	answerName: 'googleProject',
+	question: answers => `${answers.hosting == 'googlecloud' ? 'Google Cloud' : 'Firebase'} Project (If you're not sure, run ${answers.hosting == 'googlecloud' ? `'gcloud projects list'`.italic.bold : `'firebase list'`.italic.bold} to list all your available projects. If you're deploying locally, this question does not really matter): (${answers.projectName.toLowerCase()}) `.cyan,
+	answerName: 'project',
 	defaultValue: answers => answers.projectName,
 	execute: {
 		onSuccess: answer => answer.toLowerCase()
 	},
 	files: ['appconfig.json']
 },{
+	skip: answers => answers.hosting == 'firebase',
 	question: answers => `Google Cloud Function bucket: (${sanitizeBucket(answers.projectName)}) `.cyan,
 	answerName: 'bucket',
 	defaultValue: answers => answers.projectName,
--- templates/README.md
+++ templates/README.md
@@ -1,3 +1,4 @@
+
 # appconfig.json
 The appconfig.json allows you to configure your mini server to achieve 2 main goals:
 1. Configuring CORS using the _headers_ property.
@@ -13,27 +14,37 @@ Let's have a look to a basic example:
 		"Access-Control-Allow-Origin": "*",
 		"Access-Control-Max-Age": "1296000"
 	},
+	"hosting": "googlecloud",
 	"env": {
+		"active": "default",
 		"default": {
 			"functionName": "YourAwesomeAPI",
 			"trigger": "--trigger-http",
 			"entryPoint": "main",
-			"googleProject": "your-google-project",
+			"project": "your-google-project",
 			"bucket": "your-bucket"
 		},
 		"build": {
 			"functionName": "YourAwesomeAPI",
 			"trigger": "--trigger-http",
 			"entryPoint": "main",
-			"googleProject": "your-google-project",
+			"project": "your-google-project",
 			"bucket": "your-bucket"
 		}
 	}
 }
 ```
 
-The above configuration will:
+The above configuration can be read as follow:
+
+_**headers**_
 - Restrict access to your API to the following verbs only: _GET, HEAD, OPTIONS, POST_. A PUT would through a 403 error. 
 - Allow the client to pass the following headers in its request: _Authorization, Origin, X-Requested-With, Content-Type, Accept_. 
 - Allow access to your API to any client (aka Cross-Origin Resource Sharing). That's what the '*' means.
 - Cache any preflight request in the clients for maximum 1296000 seconds (i.e. 15 days). However, the caching time to live is ultimately controlled by each browser. They might not allow such a long period.
+
+_**hosting**_
+- The web server can either be hosted on Google Cloud Functions ("hosting": "googlecloud") or Firebase ("hosting": "firebase").
+
+_**env**_
+- "active". This flag is set to _default_, which means that the current active environment is the "default" one. If it is set to _build_, the "build" settings would be used.
